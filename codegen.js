f21c82b772b0aa6dd3ea04de21bbb5df132bf3ff
Chris Tarquini <chris@ilsken.com>
2013-07-20 00:19:38 -0400

    fixed source map support! moved code generation and source mapping stuff to CodeGenerator stream

---
+++ codegen.js
@@ -0,0 +1,118 @@
+/* jshint node: true, asi: true, laxcomma: true, strict: false */
+var SourceNode = require('source-map').SourceNode;
+var TransformStream = require('stream').Transform;
+exports = module.exports = CodeGenerator;
+var lineBreak = /\r\n|[\n\r\u2028\u2029]/g
+
+function CodeGenerator(options){
+	if(typeof options == 'string'){
+		options = {source: options};
+	}
+	if(!options.source) throw new Error('source is required');
+	Object.keys(options).forEach(function(key){
+		this[key] = options[key]
+	}, this)
+	this.lines = this.source.split(lineBreak).map(function(line){
+		return line.split('')
+	})
+	this.sourceName = options.sourceName || ('javascript:' + encodeURIComponent(options.source));
+	options.decodeStrings = false;
+	TransformStream.call(this, options);
+	this._readableState.objectMode = false;
+  this._writableState.objectMode = true;
+	this.mappings = []
+}
+
+CodeGenerator.prototype = Object.create(TransformStream.prototype);
+
+CodeGenerator.prototype.getSourceFor = function(node){
+	var start  = node.loc.start
+	  , end    = node.loc.end
+	  , lines  = this.lines.slice(start.line-1, end.line)
+	  , chunks = []
+
+	//console.log('getting source at ', start, end)
+	//console.log('lines are ', lines)
+
+	eachLine(lines, start, end, appendTo(chunks))
+	return chunks.join('')
+}
+
+CodeGenerator.prototype._transform = function(chunk, replace, callback){
+	var start  = chunk.loc.start
+	  , end    = chunk.loc.end
+	  , sourceName = this.sourceName
+	  , source = chunk.source !== undefined ? chunk.source : this.getSourceFor(chunk);
+	
+	var lines = this.lines.slice(start.line-1, end.line)
+	lines.forEach(function(line, i){
+		if(!line) return;
+		var x = i === 0 ? start.column : 0
+		var y = i == lines.length-1 ? end.column : line.length - 1;
+		var firstLine = i === 0;
+
+		for(var col = x; col < y; col++){
+			if (firstLine && col === x) {
+				line[col] = new SourceNode(start.line, start.column, sourceName, source, chunk.name)
+			} else{
+				delete line[col];
+			}
+		}
+	})
+
+	return callback()
+};
+
+CodeGenerator.prototype._flush = function (callback) {
+	var mappings = this.mappings
+		, debug = this.debug
+		, sourceName = this.sourceName
+		, rootNode =  new SourceNode(1, 0, sourceName, this.lines.map(function(line){
+			if(line) {
+				return line.concat(['\n'])
+			}
+			else return '\n';
+		}));
+
+	if(debug) {
+		rootNode.setSourceContent(sourceName, this.source)
+	}
+	
+	var result = rootNode.toStringWithSourceMap({
+		file: this.generatedName || this.sourceName,
+		sourceRoot: this.sourceRoot
+	});
+
+	this.push(result.code);
+	if(debug) {
+		this.push(['\n//@ sourceMappingURL=data:application/json;base64,',
+							 base64(result.map.toString())
+							 ].join(''))
+	}
+
+	callback()
+}
+
+function eachLine(lines, start, end, callback){
+	lines.forEach(function(line, i){
+		if(!line) return;
+		var x = i === 0 ? start.column : 0
+		var y = i === lines.length-1 ? end.column : undefined;
+		if(x !== 0 || y !== undefined){
+				line = line.slice(x, y)
+		}
+		callback(line, i)
+	})
+}
+
+function appendTo(chunks){
+	return function(line){
+		line.forEach(function(sourceNode){
+			if(sourceNode) chunks.push(sourceNode.toString())
+		})
+		chunks.push('\n')
+	}
+}
+function base64(str){
+	return (typeof btoa === 'function') ? btoa(str) : new Buffer(str).toString('base64')
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var parse = require('esprima').parse;
-var SourceNode = require('source-map').SourceNode;
+var CodeGenerator = require('./codegen');
 
 var objectKeys = Object.keys || function (obj) {
     var keys = [];
@@ -17,9 +17,7 @@ var isArray = Array.isArray || function (xs) {
     return Object.prototype.toString.call(xs) === '[object Array]';
 };
 
-var base64 = (typeof btoa === 'function') ? btoa : function(str){
-    return new Buffer(str).toString('base64')
-}
+
 module.exports = function (src, opts, fn) {
     if (typeof opts === 'function') {
         fn = opts;
@@ -31,27 +29,25 @@ module.exports = function (src, opts, fn) {
         delete opts.source;
     }
     src = src === undefined ? opts.source : src;
-    opts.range = true;
+    //opts.range = true;
     // required for source maps
-    if(opts.debug)
-        opts.loc = true;
+    // new code generator uses line/col instead of range
+    opts.loc = true;
     if (typeof src !== 'string') src = String(src);
     
     var ast = parse(src, opts);
+
     
-    var result = {
-        chunks : src.split(''),
-        toString : function () { return result.chunks.join('') },
-        inspect : function () { return result.toString() }
-    };
-    var index = 0;
-    var sourceMap = opts.debug ? new SourceNode() : undefined;
-    var name = opts.sourceName || 'source.js';
-    if(opts.debug)
-        sourceMap.setSourceContent('source.js', src);
+    var code = new CodeGenerator({
+        debug: opts.debug,
+        source: src,
+        sourceName: opts.sourceName,
+        generatedName: opts.generatedName,
+        sourceRoot: opts.sourceRoot
+    });
     
     (function walk (node, parent) {
-        insertHelpers(node, parent, result.chunks, sourceMap);
+        insertHelpers(node, parent, code);
         
         forEach(objectKeys(node), function (key) {
             if (key === 'parent') return;
@@ -65,33 +61,24 @@ module.exports = function (src, opts, fn) {
                 });
             }
             else if (child && typeof child.type === 'string') {
-                insertHelpers(child, node, result.chunks, sourceMap);
+                insertHelpers(child, node, code);
                 walk(child, node);
             }
         });
         fn(node);
     })(ast, undefined);
-    if(opts.debug){
-        sourceMap = sourceMap.toStringWithSourceMap({
-            file: opts.fileName || 'generated.js'
-        })
-        result.map = sourceMap.map;
-        result.chunks.push('\n//@ sourceURL=data:application/json;base64,' + base64(sourceMap.toString()));
-    }
     
-    
-    return result;
+    code.end();
+    // code generator is all sync anyway
+    return code.read().toString();
 };
  
-function insertHelpers (node, parent, chunks, sourceMap) {
-    if (!node.range) return;
-    
+function insertHelpers (node, parent, code) {
+    if(!node.loc) return;
     node.parent = parent;
     
     node.source = function () {
-        return chunks.slice(
-            node.range[0], node.range[1]
-        ).join('');
+        return code.getSourceFor(node);
     };
     
     if (node.update && typeof node.update === 'object') {
@@ -106,13 +93,10 @@ function insertHelpers (node, parent, chunks, sourceMap) {
     }
     
     function update (s) {
-        chunks[node.range[0]] = s;
-        if(sourceMap) {
-            var location = node.loc.start;
-            sourceMap.add(new SourceNode(location.line, location.column, 'source.js', s));
-        }
-        for (var i = node.range[0] + 1; i < node.range[1]; i++) {
-            chunks[i] = '';
-        }
+       code.write({
+            source: s,
+            loc: node.loc,
+            name: node.name
+       });
     };
 }
